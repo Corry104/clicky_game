7d1b0e166c85bcf43c394c93c0eed84c13fc1ea3
Corrado Alfano <corrado.alfano10@gmail.com>
2019-02-04 12:00:46 -0800

    Updates

---
+++ precache-manifest.64bfd1086bfe2124bebe58f4362d3930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51ea6fe05a3631faefcd",
+    "url": "/Corry104/clicky_game/static/css/main.60dae25a.chunk.css"
+  },
+  {
+    "revision": "51ea6fe05a3631faefcd",
+    "url": "/Corry104/clicky_game/static/js/main.51ea6fe0.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Corry104/clicky_game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "d688976b3264426b919b",
+    "url": "/Corry104/clicky_game/static/js/runtime~main.d688976b.js"
+  },
+  {
+    "revision": "31dbc0f894687548cbcdf092549186bc",
+    "url": "/Corry104/clicky_game/index.html"
+  }
+];
